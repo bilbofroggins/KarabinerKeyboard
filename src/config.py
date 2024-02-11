10b001f59046c60c814a17229722c443991841a0
Patrick Cunniff <pjcfifa@gmail.com>
2024-02-10 21:22:30 -0500

    debug config
    Signed-off-by: Patrick Cunniff <pjcfifa@gmail.com>

--- src/config.py
+++ src/config.py
@@ -1,6 +1,32 @@
+import sys
+
+
 class Config:
     window_width = 960
     window_height = 540
     font_size = 20
     generic_padding = 20
     karabiner_file = '~/.config/karabiner/karabiner.json'
+
+class Config_Debug:
+    karabiner_file = '~/.config/karabiner/karabiner_debug.json'
+
+
+
+
+def merge_configs_if_debug(debug_mode):
+    if debug_mode:
+        # Iterate through each attribute in Config_Debug
+        for attribute_name in dir(Config_Debug):
+            if not attribute_name.startswith("__"):  # Ignore magic methods and attributes
+                # Get attribute value from Config_Debug
+                attribute_value = getattr(Config_Debug, attribute_name)
+                # Set/Update this attribute in Config
+                setattr(Config, attribute_name, attribute_value)
+
+# Example usage
+if not getattr(sys, 'frozen', False):
+    debug_mode = True
+else:
+    debug_mode = False
+merge_configs_if_debug(debug_mode)
