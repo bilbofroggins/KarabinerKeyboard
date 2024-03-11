9cd2b6a58baf352dfc437e352b90b4a324217ced
Patrick Cunniff <pjcfifa@gmail.com>
2024-03-11 19:50:58 -0400

    Version 0.0.11
    Signed-off-by: Patrick Cunniff <pjcfifa@gmail.com>

--- karabinerkeyboard/builder.py
+++ karabinerkeyboard/builder.py
@@ -17,7 +17,34 @@ def modify_bundle_spec(spec_file, icon_path, bundle_id):
     with open(spec_file, 'w') as file:
         file.write(modified_content)
 
+def update_version_number():
+    filename = 'src/versions/version.py'
+
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('__version__'):
+                version_line_index = i
+                version_string = line.split('=')[1].strip().strip("'").strip('"')
+                break
+
+    version_parts = version_string.split('.')
+    version_parts[-1] = str(
+        int(version_parts[-1]) + 1)
+    updated_version_string = '.'.join(version_parts)
+
+    updated_version_line = f"__version__ = '{updated_version_string}'\n"
+
+    lines[version_line_index] = updated_version_line
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+    print(f"Version updated to: {updated_version_string}")
+
+
 def bundle_app():
+    update_version_number()
+
     subprocess.run([
         "pyinstaller",
         "src/main.py",
